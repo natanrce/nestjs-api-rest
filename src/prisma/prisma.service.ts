15047c0daf7aa3cb936a606085e2b6779b669580
Natan Rodrigues <eusounatan@proton.me>
2023-01-22 11:11:12 -0300

    feat: create prisma user middleware

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,10 +1,29 @@
 import { PrismaClient } from '@prisma/client';
 import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
 
+import { hashSync } from 'bcrypt';
+
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
     await this.$connect();
+
+    this.$use(async (params, next) => {
+      const isCreateOrUpdate =
+        params.action == 'create' || params.action == 'update';
+
+      if (isCreateOrUpdate && params.model == 'User') {
+        const { password, ...user } = params.args.data;
+        const hashedPassword = hashSync(password, 10);
+
+        params.args.data = {
+          ...user,
+          password: hashedPassword,
+        };
+      }
+
+      return next(params);
+    });
   }
 
   async enableShutdownHooks(app: INestApplication) {
